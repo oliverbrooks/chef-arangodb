5ed7e54a1f245ddbea615fbb0efdae1d28b34779
Oliver Brooks <oli@valobox.com>
2015-03-13 11:46:37 +0000

    updated deb install with new arango package locations

--- recipes/install_deb.rb
+++ recipes/install_deb.rb
@@ -1,22 +1,16 @@
 include_recipe "apt"
 
-ubu_release = %x(lsb_release -irs).strip.sub( /\n/,'-')
+ubuntu_release = %x(lsb_release -irs).strip.sub( /\n/,'_')
 
 apt_repository "arangodb" do
-  uri "http://www.arangodb.org/repositories/"
-  components ["main"]
-  distribution ubu_release
-  key "http://www.arangodb.org/repositories/PublicKey" 
-  action :create
+  uri "http://www.arangodb.org/repositories/arangodb2/x#{ubuntu_release}"
+  distribution '/'
+  key "https://www.arangodb.com/repositories/arangodb2/x#{ubuntu_release}/Release.key"
 end
 
 # This can  fail 'cause remote  repo  don't have a i386 package
-# Must works with x86-84 bits 
+# Must works with x86-84 bits
 package "arangodb" do
-  action :upgrade
-end 
-
-service "arangod" do
-  action [ :enable, :start ]
-   subscribes :restart, resources( :template => "#{node['arangodb']['config']}" ), :immediately if node['arangodb']['manage_config_file']
+  action :install
+  version node[:arangodb][:package_version]
 end
